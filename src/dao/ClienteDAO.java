b44cc3e230caf976147edc72fe06cb62c708724f
SergioArthur115@gmail.com <182120042@senacinformatica.br>
2023-04-04 11:56:38 -0300

    Criados métodos na classe ClienteDAO

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -7,8 +7,10 @@ package dao;
 
 import conexao.Conexao;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.Cliente;
@@ -33,5 +35,46 @@ public class ClienteDAO {
             System.out.println("Erro ao cadastrar!\n"
                     + ex.getMessage());
         }
+    }
+
+    public ArrayList<Cliente> getClienteBD() {
+        Connection con = Conexao.getConexao();
+        try {
+            Statement stat = con.createStatement();
+            String sql = "Select * from clientes";
+            ResultSet rs = stat.executeQuery(sql);
+            ArrayList<Cliente> clientes = new ArrayList<>();
+            while (rs.next()) {
+                Cliente c = new Cliente();
+                c.setIdCliente(rs.getInt("idcliente"));
+                c.setNomeCliente(rs.getString("nome"));
+                c.setCpf(rs.getString("cpf"));
+                clientes.add(c);
+            }
+            return clientes;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao listar!\n"
+                    + ex.getMessage());
+        }
+        return null;
+    }
+
+    public Cliente getClienteByDoc(String cpf) {
+        Connection con = Conexao.getConexao();
+        Cliente c = null;
+        try {
+            Statement stat = con.createStatement();
+            String sql = "select * from clientes where cpf='" + cpf + "'";
+            ResultSet rs = stat.executeQuery(sql);
+            while (rs.next()) {
+                c.setIdCliente(rs.getInt("idcliente"));
+                c.setNomeCliente(rs.getString("nome"));
+                c.setCpf(rs.getString("cpf"));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Erro ao consultar CPF!\n"
+                    + ex.getMessage());
+        }
+        return c;
     }
 }
